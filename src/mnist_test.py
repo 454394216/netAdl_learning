e3aab7162ee737d3007c7b88aee4f2dac3a67ddc
zy <zy@126.com>
2018-02-06 13:58:34 +0800

    test commit

--- src/mnist_test.py
+++ src/mnist_test.py
@@ -5,7 +5,7 @@ import os
 
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 #net = network.Network([784, 30, 10])
-#net = network.Network([784, 10])
+net = network.Network([784, 10])
 net = net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
 
 
